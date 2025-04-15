eb96c4ed1be74aa6439bfdd52010242612920775
kayyza <rachel.herron@live.ca>
2025-04-15 11:45:52 -0400

    Added method headers to Database class

--- CardManagementSystem/src/main/java/org/example/Database.java
+++ CardManagementSystem/src/main/java/org/example/Database.java
@@ -3,16 +3,56 @@ package org.example;
 import java.util.HashMap;
 import java.util.LinkedList;
 
-public class Database<T> {
+public class Database {
     private HashMap<String, Card> entries;
-    private LinkedList<T> entryLogs;
+    private LinkedList<Card> cardList;
+    private LinkedList<User> userList;
+    private LinkedList<Deck> deckList;
 
     public Database() {
         entries = new HashMap<>();
-        entryLogs = new LinkedList<>();
+        cardList = new LinkedList<>();
+        userList = new LinkedList<>();
+        deckList = new LinkedList<>();
     }
 
+    /*
+    adds a card to the card list
+     */
     public void addCard(Card card) {
-        entries.put(card.getCardID(), card);
+
+    }
+
+    /*
+    removes a card from the cardList
+     */
+    public void removeCard(String cardID) {
+
+    }
+
+    /*
+    adds a user to the user list
+     */
+    public void addUser(User user) {
+
+    }
+
+    /*
+    removes a user from the user list
+     */
+    public void removeUser(String userID) {
+
+    }
+    /*
+    adds a deck to the deck list
+     */
+    public void addDeck(Deck deck) {
+
+    }
+     /*
+     removes a deck from the deck list
+      */
+    public void removeDeck(String deckID) {
+
     }
 }
