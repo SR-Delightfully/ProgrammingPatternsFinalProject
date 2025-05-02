e3ee148f20c01ad79d714d9fab2ad37a052bfce5
kayyza <rachel.herron@live.ca>
2025-05-01 23:32:41 -0400

    Created JAR file

--- CardManagementSystem/src/test/java/org/example/TestDatabaseConnection.java
+++ CardManagementSystem/src/test/java/org/example/TestDatabaseConnection.java
@@ -6,7 +6,7 @@ class TestDatabaseConnection extends DatabaseConnection {
     private final Connection connection;
 
     public TestDatabaseConnection(Connection connection) {
-        super("jdbc:sqlite::memory:"); // dummy URL, won't be used
+        super(); // dummy URL, won't be used
         this.connection = connection;
     }
 
