2d414e0d2b6a7ab50a8e0d06d5385ae116e59c13
kayyza <rachel.herron@live.ca>
2025-05-01 23:22:34 -0400

    Documentation

--- CardManagementSystem/src/main/java/org/example/Main.java
+++ CardManagementSystem/src/main/java/org/example/Main.java
@@ -3,6 +3,17 @@ package org.example;
 
 import javax.swing.*;
 
+// -------------------------------------------------------
+// Final Project
+// Written by: Rachel Herron (1945431) & Sabrina Robinson (1763514)
+
+//   - [Rachel]: Implemented API integration, and database logic.
+//   - [Sabrina]: Designed and implemented the GUI and internationalization features.
+//
+// For “Programming Patterns” Section (Section 1) – Winter 2025
+// -------------------------------------------------------
+
+
 // This project sees the development of a simple swing java application in which we use a database to manage our
 // application's data. The data itself is pulled from various API's for the various collectible/trading card games
 // that we support. These games include Magic: The gathering, Pokémon and there will be future support for Yugioh players.
